17aaba374738ba9820ce00d8e231326581747341
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-01-06 14:06:46 +0100

    Add SideMenu

---
+++ lib/widgets/side_menu.dart
@@ -0,0 +1,62 @@
+export 'side_menu.dart';
+
+import 'package:flutter/material.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: 280,
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Image.asset(
+                  'lib/assets/Spotify-F-DL.png',
+                  height: 55,
+                  filterQuality: FilterQuality.high,
+                ),
+              )
+            ],
+          ),
+          _SideMenuIconTab(iconData: Icons.home, title: 'Home', onTap: () {}),
+        ],
+      ),
+    );
+  }
+}
+
+class _SideMenuIconTab extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+  final VoidCallback onTap;
+  const _SideMenuIconTab({
+    Key? key,
+    required this.iconData,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        iconData,
+        color: Theme.of(context).iconTheme.color,
+        size: 28,
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyText1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      onTap: onTap,
+    );
+  }
+}
