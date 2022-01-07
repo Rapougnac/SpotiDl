729f18927310441d373ba8297c656e4d569611da
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-01-07 18:57:30 +0100

    Added `NotFound` exeption

--- lib/widgets/screens/home_page.dart
+++ lib/widgets/screens/home_page.dart
@@ -291,3 +291,10 @@ Directory? getMusicDirectory() {
     throw UnsupportedError('Platform not supported');
   }
 }
+
+/// A not found exception, thrown when a song is not found, or the user cancels the download
+class NotFound extends Error {
+  /// The message to display when the error occurs.
+  final String message;
+  NotFound(this.message);
+}
