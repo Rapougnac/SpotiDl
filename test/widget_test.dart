d5510b29f6ac9bb8db55a4b1d43b9740c8dfdf37
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-01-12 09:57:53 +0100

    Updated tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:spotidl/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Tap on the search bar', (WidgetTester tester) async {
     await tester.pumpWidget(const SpotifyDownloader());
 
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('Search for a song'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('Search for a song'));
     await tester.pump();
 
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
   });
 }
