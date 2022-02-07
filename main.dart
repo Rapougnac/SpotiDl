7f2d75b7ae27c6c70daa36a8954162883139f806
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-02-07 16:22:33 +0100

    Oops, forgot to delete this test file

--- main.dart
+++
@@ -1,9 +0,0 @@
-void main(List<String> args) {
-  print(convertStringTimeToSeconds('01:15'));
-}
-
-int convertStringTimeToSeconds(String time) {
-  final timeSplit = time.split(':');
-  final seconds = int.parse(timeSplit[0]) * 60 + int.parse(timeSplit[1]);
-  return seconds;
-}
